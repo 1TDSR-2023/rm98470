de5b1af7ff8d5248b131873d70b2da533fcb84f1
bruno1098 <rm98470@fiap.com.br>
2023-05-03 22:56:05 -0300

    criando botão rgb irado com addEvent, e vendo Math

--- js/script.js
+++ js/script.js
@@ -7,11 +7,37 @@
 //regra1: INICIALIZAR NA DECLARAÇÃO
 //REGRA2: NÃO ALTERAR O SEU VALOR DURANTE O FLUXO DO PROGRAMA
 //constante, usado para declarar dados que vá utilizar com frequencia e que NÃO PODE ser alterado
+//
+//var nome1 = "Bruno";
+//
+//if(nome1 != "Bruno"){
+//    let nome1 = "Antunes"
+//}
+//console.log(nome1);
 
-var nome1 = "Bruno";
+let botao = document.getElementById("meu-btn");
+console.log(botao.textContent);
 
-if(nome1 != "Bruno"){
-    let nome1 = "Antunes"
-}
-console.log(nome1);
+//botao.addEventListener("EVENTO","FUNÇÃO DE CALL BACK");
+botao.addEventListener("click",function(){
+    //console.log(this.textContent);
+    //função Math:Random = retorna um número aleatório ENTRE 0 E 1
+    //            floor = arredonda um número para baixo
+    //            ceil = arrendona um número para cima
+    //            round = arrendona um número aleatoriamente
 
+    let r=0,g=0,b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    //this.setAttribute("NOME DO ATRIBUTO", "VALOR PARA ESTE ATRIBUTO");
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+
+});
+
+//ESTUDAR: 
+//querySelector
+//querySelectorAll
+//Array
+//Estruturas de repetição
