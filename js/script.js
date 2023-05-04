821fc3ad17aea0948fb3fba99ade5890e4070dc5
bruno1098 <rm98470@fiap.com.br>
2023-05-03 21:45:44 -0300

    diferença entre var, let e const

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+//var nome1 = "Bruno";
+//let nome2 = "Derick";
+//para declarar variávei simples, pode declarar números, strings, objetos.
+//principal diferença entre let e var é o hosting
+
+//const PI = 3.14; 
+//regra1: INICIALIZAR NA DECLARAÇÃO
+//REGRA2: NÃO ALTERAR O SEU VALOR DURANTE O FLUXO DO PROGRAMA
+//constante, usado para declarar dados que vá utilizar com frequencia e que NÃO PODE ser alterado
+
 var nome1 = "Bruno";
-let nome2 = "Derick";
-const nome3 = "Eu";
+
+if(nome1 != "Bruno"){
+    let nome1 = "Antunes"
+}
+console.log(nome1);
+
