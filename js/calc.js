68328910ecc53c0f154c5f43096b4703f480246e
bruno1098 <rm98470@fiap.com.br>
2023-05-12 22:59:08 -0300

    desafio e arrow function

---
+++ js/calc.js
@@ -0,0 +1,12 @@
+
+function insereNumeroNoVisor(botao,visor){
+    visor.value += botao.value
+
+}
+
+const calculoDoVisor = (visor)=>{
+    visor.value = eval(visor.value);
+}
+//()=>{}
+//desafio 3
+//impeça a entrada de mais de uma vez do operador matematico
