be77a4830ed295cb4aa3e776e092a82ee04ff8a6
bruno1098 <rm98470@fiap.com.br>
2023-05-17 22:59:17 -0300

    validação

--- js/login.js
+++ js/login.js
@@ -17,6 +17,7 @@ inputEmail.addEventListener("keyup", ()=>{
     //inputEmail.setAttribute("style","outline-color: #ff0000;");
 });
 
+const inputSenha = document.querySelector("#idSenha");
 const eyePass = document.querySelector(".fa");
 
 eyePass.addEventListener("click", ()=>{
@@ -28,4 +29,77 @@ eyePass.addEventListener("click", ()=>{
        inputSenha.setAttribute("type","password");
        eyePass.setAttribute("class","fa fa-eye");
     }
+});
+
+//creation of objects in js
+//const usuario1 = {
+// nomeCompleto : "Lucas das Couves",
+// emailUsuario : "lucas@gmail.com",
+// senhaUsuario : "123456"
+//}
+
+//list of users
+
+let listaDeUsuarios = [
+{
+    nomeCompleto : "Lucas das Couves",
+    emailUsuario : "lucas@gmail.com",
+    senhaUsuario : "123456"
+    },
+    {
+    nomeCompleto : "Juan Pelego",
+    emailUsuario : "Juan@gmail.com",
+    senhaUsuario : "123456"
+    },
+    {
+    nomeCompleto : "Matheuzin Delgado",
+    emailUsuario : "Matheuzin@gmail.com",
+    senhaUsuario : "123456"
+    },
+    {
+    nomeCompleto : "Derick Maluco",
+    emailUsuario : "Derick@gmail.com",
+    senhaUsuario : "123456"
+   
+}
+];
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+botaoSubmit.addEventListener("click", ()=>{
+    //creating an validation object
+    let userValidado = {
+        emailUsuarioValidado : inputEmail.value,
+         senhaUsuarioValidado : inputSenha.value
+    }
+//criando uma interação com a lista de objetos para realizar a validação
+//com o usuarioValidado
+try{
+    listaDeUsuarios.forEach((usuario)=>{
+    //realizando a validação
+    //comparando cada objeto da lista com o objeto monstado com os dados que vieram do form
+    if(userValidado.emailUsuarioValidado == usuario.emailUsuario && userValidado.senhaUsuarioValidado == usuario.senhaUsuario){
+        userValidado = usuario;
+        throw "VALIDADO";
+}
+});
+
+throw "senha errada";
+}catch(msg){
+
+    const msgStatus = document.querySelector("#msgStatus");
+
+    if(msg == "VALIDADO"){
+        msgStatus.setAttribute("style","color:#00ff00");
+        msgStatus.innerHTML = `<span><strong>Usuário: ${userValidado.nomeCompleto} Login efetuado com Sucesso!</strong></span>`;
+
+        //Redirect
+        
+
+    }else{
+        msgStatus.setAttribute("style","color:#ff0000");
+        msgStatus.innerHTML = "<span><strong>Nome de usuário ou senha inválidos...</strong></span>";
+    }
+}
+   
 });
